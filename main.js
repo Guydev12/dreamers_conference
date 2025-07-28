dfb5be6b6196e9eff05765fd9b8fee3a862f339e
Guydev12 <josephguivens23@gmail.com>
2025-07-28 16:33:33 -0400

    refactor the header

--- main.js
+++ main.js
@@ -20,3 +20,25 @@ const observer = new IntersectionObserver(
 cards.forEach((card) => {
   observer.observe(card);
 });
+
+const countDate = new Date("2025-09-07T09:00:00").getTime();
+setInterval(() => {
+  const now = new Date().getTime();
+  const gap = countDate - now;
+    if (gap <= 0) {
+    document.getElementById("countDown").innerText = "Conférence en cours ! 🎉";
+    return;
+  }
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+   const days = Math.floor(gap / day);
+  const hours = Math.floor((gap % day) / hour);
+  const minutes = Math.floor((gap % hour) / minute);
+  const seconds = Math.floor((gap % minute) / second);
+
+  document.getElementById("countDown").innerText =
+    `${days}j ${hours}h ${minutes}m ${seconds}s restants`;
+}, 1000);
